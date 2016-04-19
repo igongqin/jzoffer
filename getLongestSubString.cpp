a4f65f1859adc9a1c9420a14c830b876753dd270
igongqin <igongqin@qq.com>
2016-04-19 14:29:24 +0800

    Create getLongestSubString.cpp

---
+++ getLongestSubString.cpp
@@ -0,0 +1,26 @@
+//求最大公共子串
+int getLongestSubString(char* query,char* text){
+	int imax = INT_MIN;
+	int m = strlen(query);
+	int n = strlen(text);
+	vector<vector<int> > temp(m,vector<int>(n,0));//创建一个m行n列的全0二维数组
+	
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(query[i]==text[j])
+			{
+				temp[i][j] = 1;
+			}
+			else
+			{
+				temp[i][j]=temp[i-1][j-1]+1;
+			}
+			
+			imax = max(imax,temp[i][j]);
+		}
+	}
+	
+	return imax;
+}
